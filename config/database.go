29929e24ca72c76c83d2014c32bbc64122befa6f
Aman <arl0817osho@gmail.com>
2025-03-20 00:14:11 +0530

    fix: remove sslmode requirement from database connection string

--- config/database.go
+++ config/database.go
@@ -19,7 +19,7 @@ func ConnectDB() {
 	if dbPassword == "" {
 		log.Fatal("Missing DB_PASSWORD environment variable")
 	}
-	dsn := fmt.Sprintf("host=closeloopdb.postgres.database.azure.com user=postgres password=%s dbname=postgres port=5432 sslmode=require", dbPassword)
+	dsn := fmt.Sprintf("host=closeloopdb.postgres.database.azure.com user=postgres password=%s dbname=postgres port=5432 ", dbPassword)
 
 	//DOCS
 	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
